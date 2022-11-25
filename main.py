f2a5296d2d03cc773fba7653107df90a09495ea0
Steve <39433211+stevencoveta@users.noreply.github.com>
2022-11-25 02:15:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ input = st.sidebar.text_input('Write username')
 df = get_tweets(input)
 
 #st.dataframe(df.head())
-st.subheader(f"We've got {len(df)} tweets ranging from {df.date[0]} to {df.date[-1]}")
+st.subheader(f"We've got {len(df)} tweets ranging from {df.date[0]}")
 def filter_keywords(a,b):
     text = []
     dt = []
